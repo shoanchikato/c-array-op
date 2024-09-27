3ae7f838807f1279fa12fb2fd0e49ac2ec9076a1
Shoan Chikato <sjchikato@gmail.com>
2024-09-27 03:51:46 +0200

    add tests for the additional four functions
    - add tests for push_front, push_back, pop_front and pop_back

--- test/test.h
+++ test/test.h
@@ -19,6 +19,11 @@ int test_delete_at_len();
 int test_delete_at_element();
 int test_get_element();
 
+int test_push_front();
+int test_push_back();
+int test_pop_front();
+int test_pop_back();
+
 int test_insert_len() {
   // arrange
   const char *test_name = "test_insert_len";
@@ -230,6 +235,154 @@ int test_get_element() {
   return EXIT_SUCCESS;
 }
 
+int test_push_front() {
+  // arrange
+  const char *test_name = "test_push_front";
+  int *nums = NULL;
+  size_t len = 0;
+  int value[] = {13, 24, 35, 46};
+
+  int expect[] = {13, 24, 35, 46};
+  size_t expect_len[] = {1, 2, 3, 4}; 
+
+  // act
+  for (int i = 0; i < 4; i++) {
+    int n = push_front((void **)&nums, &len, &value[i], sizeof(int));
+    if (n != 0) {
+      return unexpected_error(test_name);
+    }
+    int got = nums[0];
+
+    // assert
+    if (expect_len[i] != len) {
+      printf("%s: len test: expect %zu, got %zu\n", test_name, expect_len[i], len);
+      return EXIT_FAILURE;
+    }
+
+    if (expect[i] != got) {
+      printf("%s: element test: expect %d, got %d\n", test_name, expect[i], got);
+      return EXIT_FAILURE;
+    }
+  }
+
+  free(nums);
+  return EXIT_SUCCESS;
+}
+
+int test_push_back() {
+  // arrange
+  const char *test_name = "test_push_back";
+  int *nums = NULL;
+  size_t len = 0;
+  int value[] = {13, 24, 35, 46};
+
+  int expect[] = {13, 24, 35, 46};
+  size_t expect_len[] = {1, 2, 3, 4}; 
+
+  // act
+  for (int i = 0; i < 4; i++) {
+    int n = push_back((void **)&nums, &len, &value[i], sizeof(int));
+    if (n != 0) {
+      return unexpected_error(test_name);
+    }
+    int got = nums[len-1];
+
+    // assert
+    if (expect_len[i] != len) {
+      printf("%s: len test: expect %zu, got %zu\n", test_name, expect_len[i], len);
+      return EXIT_FAILURE;
+    }
+
+    if (expect[i] != got) {
+      printf("%s: element test: expect %d, got %d\n", test_name, expect[i], got);
+      return EXIT_FAILURE;
+    }
+  }
+
+  free(nums);
+  return EXIT_SUCCESS;
+}
+
+int test_pop_front() {
+  // arrange
+  const char *test_name = "test_pop_front";
+  size_t len = 4;
+  int *nums = (int *)calloc(len, len * sizeof(int));
+  nums[0] = 13;
+  nums[1] = 24;
+  nums[2] = 35;
+  nums[3] = 46;
+
+  int expect[4] = {13, 24, 35, 46};
+  size_t expect_len[] = {3, 2, 1, 0}; 
+
+  int *got = 0;
+
+  // act
+  for (int i = 0; i < 4; i++) {
+    got = (int*) pop_front((void **)&nums, &len, sizeof(int));
+    if (got == NULL) {
+      return unexpected_error(test_name);
+    }
+
+    // assert
+    if (expect_len[i] != len) {
+      printf("%s: len test: expect %zu, got %zu\n", test_name, expect_len[i], len);
+      return EXIT_FAILURE;
+    }
+
+    if (expect[i] != *got) {
+      printf("%s: element test: expect %d, got %d\n", test_name, expect[i], *got);
+      return EXIT_FAILURE;
+    }
+
+    free(got);
+  }
+
+  free(nums);
+  return EXIT_SUCCESS;
+}
+
+int test_pop_back() {
+  // arrange
+  const char *test_name = "test_pop_back";
+  size_t len = 4;
+  int *nums = (int *)calloc(len, len * sizeof(int));
+  nums[0] = 13;
+  nums[1] = 24;
+  nums[2] = 35;
+  nums[3] = 46;
+
+  int expect [] = {46, 35, 24, 13};
+  size_t expect_len[] = {3, 2, 1, 0}; 
+
+  int *got = 0;
+
+  // act
+  for (int i = 0; i < 4; i++) {
+    got = (int*) pop_back((void **)&nums, &len, sizeof(int));
+    if (got == NULL) {
+      return unexpected_error(test_name);
+    }
+
+    // assert
+    if (expect_len[i] != len) {
+      printf("%s: len test: expect %zu, got %zu\n", test_name, expect_len[i], len);
+      return EXIT_FAILURE;
+    }
+
+    if (expect[i] != *got) {
+      printf("%s: element test: expect %d, got %d\n", test_name, expect[i], *got);
+      return EXIT_FAILURE;
+    }
+
+    free(got);
+  }
+
+  free(nums);
+  return EXIT_SUCCESS;
+}
+
 int exec() {
   int n = 0;
 
@@ -258,6 +411,22 @@ int exec() {
     return EXIT_FAILURE;
 
   n = test_get_element();
+  if (n != 0)
+    return EXIT_FAILURE;
+
+  n = test_push_front();
+  if (n != 0)
+    return EXIT_FAILURE;
+
+  n = test_push_back();
+  if (n != 0)
+    return EXIT_FAILURE;
+
+  n = test_pop_front();
+  if (n != 0)
+    return EXIT_FAILURE;
+
+  n = test_pop_back();
   if (n != 0)
     return EXIT_FAILURE;
 
